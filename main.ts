3d7935bfe0e1d74d41ab7a554b7acb92c61253e6
Yaroslav Kulyk <ykulik@hotmail.com>
2023-01-23 12:50:48 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,17 +3,17 @@ basic.forever(function () {
     radio.sendValue("mgx", input.acceleration(Dimension.X))
     radio.sendValue("mgy", input.acceleration(Dimension.Y))
     if (input.acceleration(Dimension.Y) <= 0) {
-        led.plot(2, 0)
         led.unplot(2, 4)
+        led.plot(2, 0)
     } else {
-        led.plot(2, 4)
         led.unplot(2, 0)
+        led.plot(2, 4)
     }
     if (input.acceleration(Dimension.X) >= 0) {
-        led.plot(4, 2)
         led.unplot(0, 2)
+        led.plot(4, 2)
     } else {
-        led.plot(0, 2)
         led.unplot(4, 2)
+        led.plot(0, 2)
     }
 })
