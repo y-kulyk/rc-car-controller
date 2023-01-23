37e92adf3dc2f8a065e5e55acc9a478f07e05c8b
Yaroslav Kulyk <ykulik@hotmail.com>
2023-01-23 20:23:14 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
+let mg_y = 0
+let mg_x = 0
 radio.setGroup(1)
 basic.forever(function () {
     radio.sendValue("mgx", input.acceleration(Dimension.X))
+    mg_x = input.acceleration(Dimension.X)
+    serial.writeValue("mgx", mg_x)
+    basic.pause(100)
     radio.sendValue("mgy", input.acceleration(Dimension.Y))
+    mg_y = input.acceleration(Dimension.Y)
+    serial.writeValue("mgy", mg_y)
+    serial.writeLine("Two values sent")
+    basic.pause(100)
     if (input.acceleration(Dimension.Y) <= 0) {
         led.unplot(2, 4)
         led.plot(2, 0)
